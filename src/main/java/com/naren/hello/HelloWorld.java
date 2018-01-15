89810141dd4eef12d4066017bcf4cbbd23be328c
Panda Narendranath <narendranath.panda@nokia.com>
2018-01-15 21:49:30 +0530

    Initial Files

---
+++ src/main/java/com/naren/hello/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.naren.hello;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		Greet greet = new Greet();
+		System.out.println(greet.sayHello("Main"));
+
+	}
+
+}
